e056e6c615732483e4dfb8bc7fb45cbc026928d5
shshivam1520-dot <shshivam1520@gmail.com>
2026-02-07 03:07:45 +0530

    Create emotion_service.dart

---
+++ lib/services/emotion_service.dart
@@ -0,0 +1,11 @@
+class EmotionService {
+  // Mock AI emotion analysis
+  Map<String, int> analyzeEmotion(String audioData) {
+    return {
+      "happiness": 50,
+      "anger": 10,
+      "fear": 5,
+      "sadness": 35,
+    };
+  }
+}
